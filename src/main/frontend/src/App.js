12e6ba56853515edb319ce4c196aeb9508cdbcca
HongYeonSeung <yshong2332@gmail.com>
2023-10-17 08:02:35 +0900

    로고,아이콘 수정 및 리액트-라우터로 버튼클릭시 페이지이동 문제점 부트스크랩 사용하는 버튼이나 body쪽에 백그라운드 색상이 안먹음 이어서 확인 요청

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState,Component} from 'react';
 import axios from 'axios';
+import Main from './component/Main';
+import Login from "./component/Login";
+import Mypage from "./component/Mypage";
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 
 
-function App() {
+const App = () => {
 
   const [hello, setHello] = useState('연동 X')
 
@@ -15,11 +21,38 @@ function App() {
         .catch(error => console.log(error))
   }, []);
 
-  return (
-      <div>
-        백엔드 연동 테스트 : {hello}
-      </div>
-  );
+    return (
+        <div>
+            <hr></hr>
+            <Router>
+                <header>
+                    <div className="header-left"></div>
+                    <div className="header-right" >
+                        <Link to="/">
+                            <Button variant="primary" className="normal-button">Home</Button>
+                        </Link>
+                        <Link to="/page1">
+                            <Button variant="primary" className="normal-button">로그인</Button>
+                        </Link>
+                        <Link to="/page2">
+                            <Button variant="primary" className="normal-button">내 정보</Button>
+                        </Link>
+                    </div>
+
+                </header>
+                <hr />
+                <main>
+                    <Routes>
+                        <Route path="/" element={<Main />} />
+                        <Route path="/page1" element={<Login />} />
+                        <Route path="/page2" element={<Mypage />} />
+                    </Routes>
+                </main>
+            </Router>
+            <hr></hr>
+            백엔드 연동 테스트: {hello}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/main/frontend/src/component/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+    return(
+        <div>
+            <h1>Main page</h1>
+            <p>메인 페이지 입니다</p>
+        </div>
+    )
+}
+export default Main
