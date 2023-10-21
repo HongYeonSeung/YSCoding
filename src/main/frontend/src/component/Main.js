37d1a222a52c8c3261f0058a4b800a4adc76e845
HongYeonSeung <yshong2332@gmail.com>
2023-10-21 12:44:12 +0900

    연수 리액트 파일 합침

--- src/main/frontend/src/component/Main.js
+++ src/main/frontend/src/component/Main.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Layout from './main/Layout'
 
 const Main = () => {
     return(
         <div className='main'>
             <h1>Main page</h1>
             <p>메인 페이지 입니다</p>
+            <Layout></Layout>
         </div>
     )
 }
