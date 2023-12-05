5483d679fe83bbf871a7572ee8de3134fe51a679
Young-ys <2021100212@yeonsung.ac.kr>
2023-12-06 07:10:16 +0900

    내정보 업데이트까지 완료. 비밀번호 변경 만들기 전

--- src/main/frontend/src/components/EditProfilePage.js
+++ src/main/frontend/src/components/EditProfilePage.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useUser } from './UserContext'; // UserContext를 import
-
-const EditProfilePage = () => {
-    const { loginId, username } = useUser(); // 로그인된 사용자의 ID와 이름을 불러옴
-    const [birthdate, setBirthdate] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
-=======
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
@@ -38,41 +26,10 @@ const EditProfilePage = () => {
     };
 
     const navigate = useNavigate();
->>>>>>> c4c0d55 (내정보 업데이트까지 완료. 비밀번호 변경 만들기 전)
 
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
-<<<<<<< HEAD
-            const response = await axios.post('/api/edit-profile', { username: loginId, birthdate, email, password, phoneNumber }); // 수정된 정보를 백엔드로 전송
-            if (response.data === 'success') { // 백엔드의 응답이 'success'이면
-                alert('프로필이 성공적으로 수정되었습니다.'); // 성공 메시지를 표시
-            } else {
-                alert('프로필 수정에 실패하였습니다.'); // 실패 메시지를 표시
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    return (
-        <div>
-            <h2>프로필 수정</h2>
-            <form onSubmit={handleSubmit}>
-                이름:
-                <input type="text" value={loginId} disabled /> {/* 이름은 수정 불가능 */}
-                사용자 ID:
-                <input type="text" value={username} disabled /> {/* 사용자 ID는 수정 불가능 */}
-                생년월일:
-                <input type="date" value={birthdate} onChange={(e) => setBirthdate(e.target.value)} required />
-                {console.log("dfsdf", birthdate)}
-                이메일:
-                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-                비밀번호:
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                전화번호:
-                <input type="tel" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} required />
-=======
             await axios.post(`api/update-user-info`, userDto);
             alert('사용자 정보가 성공적으로 업데이트되었습니다.');
             navigate('/');
@@ -99,15 +56,10 @@ const EditProfilePage = () => {
                 <input type="text" name="birthdate" value={userDto.birthdate} onChange={handleInputChange} />
                 전화번호:
                 <input type="tel" name="phoneNumber" value={userDto.phoneNumber} onChange={handleInputChange} />
->>>>>>> c4c0d55 (내정보 업데이트까지 완료. 비밀번호 변경 만들기 전)
                 <button type="submit">수정 완료</button>
             </form>
         </div>
     );
-<<<<<<< HEAD
-};
-=======
 }
->>>>>>> c4c0d55 (내정보 업데이트까지 완료. 비밀번호 변경 만들기 전)
 
 export default EditProfilePage;
--- src/main/java/com/example/YSCoding/Controller/UserController.java
+++ src/main/java/com/example/YSCoding/Controller/UserController.java
@@ -1,9 +1,6 @@
 package com.example.YSCoding.Controller;
 
-<<<<<<< HEAD
-=======
 import com.example.YSCoding.Dto.SignupDTO;
->>>>>>> c4c0d55 (내정보 업데이트까지 완료. 비밀번호 변경 만들기 전)
 import com.example.YSCoding.Entity.Signup;
 import com.example.YSCoding.Repository.SignupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,12 +31,6 @@ public class UserController {
         }
     }
 
-<<<<<<< HEAD
-    @GetMapping("/api/user/{username}")
-    public Signup getUser(@PathVariable String username) {
-        return signupRepository.findByUsername(username); // 데이터베이스에서 사용자를 찾아 반환
-    }
-=======
     @PostMapping("/api/get-user-info")
     public SignupDTO getUserInfo(@RequestBody Map<String, String> request) {
         String username = request.get("username"); // 로그인된 사용자의 ID를 가져옴
@@ -80,12 +71,4 @@ public class UserController {
             return null; // 해당 아이디를 찾을 수 없음
         }
     }
-
-
-
-//    @GetMapping("/api/user/{username}")
-//    public Signup getUser(@PathVariable String username) {
-//        return signupRepository.findByUsername(username); // 데이터베이스에서 사용자를 찾아 반환
-//    }
->>>>>>> c4c0d55 (내정보 업데이트까지 완료. 비밀번호 변경 만들기 전)
 }
